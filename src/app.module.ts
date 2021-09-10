434fdd2e3367eae2a16e38d6d5309c47dbb3725c
thainn <nguyennangthai@gmail.com>
2021-09-10 15:29:51 +0700

    start

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/customer-app', { useNewUrlParser: true })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {}
